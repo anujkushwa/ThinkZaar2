11fc12312d1a2dd7b43bc163eb86d31bb86655e3
Anuj Maurya <rishabmaurya097@gmail.com>
2026-04-13 15:08:01 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold">
+        🚀 Welcome to ThinkZaar
+      </h1>
+    </div>
+  );
+}
